d8df9af848b1bf736c47ca4d8aaac48041fe1000
david alejandro cordoba <cordob0075@gmail.com>
2019-04-24 18:18:04 -0500

    se actualiza query.sql
    se corrigen algunas cositas y se terminan los puntos 5 y 6 del trabajo

--- Assignment 1/query.sql
+++ Assignment 1/query.sql
@@ -1,19 +1,27 @@
 
+-- Primer punto la imagen
+
+-- Segundo punto
 create tablespace coordinadora datafile
-'coordinadora_datafile' size 1G;
+'coordinadora_datafile' size 3G;
+
+--drop tablespace coordinadora INCLUDING CONTENTS AND DATAFILES;
+
+--select * from dba_tablespaces;
 
 -- validar como establecer este tablespace para ser usado en el sistema y el nombre del datafile
---create undo tablespace coordinadora datafile
---'DATAFILE_UNDO' size 100M;
+create undo tablespace coordinadora_undo datafile
+'DATAFILE_UNDO' size 100M;
 
 -- validar el nombre del datafile
-create bigfile tablespace coordinadorabigfile datafile
+create bigfile tablespace coordinadora_bigfile datafile
 'DATAFILE_BIGFILE' size 4G;
 
--- alteramos la session
+-- alteramos la session esto es para SQL developer 18
 alter session set "_ORACLE_SCRIPT"=true;
 
--- Creamos el usuario amartinezg
+-- Tercer punto
+-- Creamos el usuario amartinezg sin restricciones de espacio
 create user amartinezg
 identified by amartinezg
 default tablespace coordinadora;
@@ -21,19 +29,17 @@ default tablespace coordinadora;
 -- agregamos permisos de dba a amartinezg
 GRANT DBA TO amartinezg;
 
---preguntar si se agrega PASSWORD_GRACE_TIME o no
+-- Cuarto punto
 CREATE PROFILE clerk LIMIT 
     PASSWORD_LIFE_TIME 40
     SESSIONS_PER_USER 1
     IDLE_TIME 15
     FAILED_LOGIN_ATTEMPTS 3;
 
--- validar si FAILED_LOGIN_ATTEMPTS en 0 hace que no tenga ningun intento fallido por session
 CREATE PROFILE development LIMIT 
     PASSWORD_LIFE_TIME 100
     SESSIONS_PER_USER 2
-    IDLE_TIME 30
-    FAILED_LOGIN_ATTEMPTS 0;
+    IDLE_TIME 30;
     
 CREATE PROFILE operative LIMIT 
     PASSWORD_LIFE_TIME 30
@@ -42,3 +48,28 @@ CREATE PROFILE operative LIMIT
     FAILED_LOGIN_ATTEMPTS 4
     PASSWORD_REUSE_MAX 3
     PASSWORD_REUSE_TIME 30;
+    
+-- Quinto punto
+-- creamos y agregamos el usuario al perfil clerk y dams permiso para conectarse
+create user david1
+identified by david1
+default tablespace coordinadora;
+ALTER USER david1 profile clerk;
+grant create session, connect to david1;
+
+-- creamos y agregamos el usuario al perfil development y dams permiso para conectarse
+create user david2
+identified by david2
+default tablespace coordinadora;
+ALTER USER david2 profile development;
+grant create session, connect to david2;
+
+-- creamos y agregamos el usuario al perfil operative y dams permiso para conectarse
+create user david3
+identified by david3
+default tablespace coordinadora;
+ALTER USER david3 profile operative;
+grant create session, connect to david3;
+
+-- Bloqueamos el usuario david3 que es del perfil operative
+ALTER USER david3 ACCOUNT LOCK;
